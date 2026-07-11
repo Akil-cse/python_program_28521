c967a5427a4eec983328ac0dd0c47af8acc9bdf6
akil <akilm4378@gmail.com>
2026-07-11 20:13:12 +0600

    76 to 79

--- 076.py
+++ 076.py
@@ -1,3 +1,14 @@
 N = int(input("Enter fibonacci term you want (N): "))
 if N < 0:
-    print("Please enter a positive integer gr")
+    print("Please enter a positive integer greater than 0")
+elif N == 1:
+    print("The 1st fibonacci number is : 0 ")
+elif N == 2:
+    print("The 2nd fibonacci number is : 1")
+
+else:
+    a,b=0,1
+    for i in range(3,N+1):
+        c= a+b
+        a,b= b,c
+    print("The",N,"th fibonacci is:",b)
